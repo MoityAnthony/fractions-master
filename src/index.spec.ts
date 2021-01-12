2b2227aaacc55f9fdc92cd613893631170dd948d
Moity <anthony.77x@gmail.com>
2021-01-12 14:46:05 +0100

    push

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,5 +1,4 @@
-import { additionOfFraction, Fraction } from ".";
-
+import { Fraction } from ".";
 
 describe("Test add function", () => {
   it("Should return Fraction instance with numerators = 0 and denominators = 1", () => {
@@ -14,7 +13,7 @@ describe("Test add function", () => {
     const expectedResult = new Fraction(0, 1);
 
     expect(result).toEqual(expectedResult);
-  })
+  });
 
   it("Should return Fraction instance with numerator FractionA = 1 and denominators = 1", () => {
     // Given
@@ -28,7 +27,7 @@ describe("Test add function", () => {
     const expectedResult = new Fraction(1, 1);
 
     expect(result).toEqual(expectedResult);
-  })
+  });
 
   it("Should return Fraction instance with numerators = 1 and denominators = 1", () => {
     // Given
@@ -42,7 +41,7 @@ describe("Test add function", () => {
     const expectedResult = new Fraction(2, 1);
 
     expect(result).toEqual(expectedResult);
-  })
+  });
 
   it("Should return Fraction instance with numerators = 1 and denominators = 6", () => {
     // Given
@@ -53,10 +52,10 @@ describe("Test add function", () => {
     const result = Fraction.add(FractionA, FractionB);
 
     // Then
-    const expectedResult = new Fraction(2, 6);
+    const expectedResult = new Fraction(12, 36);
 
     expect(result).toEqual(expectedResult);
-  })
+  });
 
   it("Should return Fraction instance with numerators = 1 and denominators is not equal", () => {
     // Given
@@ -70,6 +69,26 @@ describe("Test add function", () => {
     const expectedResult = new Fraction(4, 3);
 
     expect(result).toEqual(expectedResult);
-  })
+  });
+
+
+  it("Should return Fraction instance with numerators = 1 and denominators is not equal", () => {
+    // Given
+    const fractionA = new Fraction(1, 2);
+    const fractionB = new Fraction(1, 3);
+
+    // When
+    const result = Fraction.add(fractionA, fractionB);
+
+    /**
+     *  1/1 + 1/3 = 4/3
+     *  1/2 + 1/3 = 3/6 + 2/6 = 5/6
+     */
+
+      // Then
+    const expectedResult = new Fraction(5, 6);
+
+    expect(result).toEqual(expectedResult);
+  });
 
 });
